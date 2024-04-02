3925fb1f5103e65a713be2c1a2f04dbf9df72861
walter souza <wszin06@gmail.com>
2024-04-02 16:55:09 -0300

    criando meu primeiro chatbot

---
+++ main.py
@@ -0,0 +1,43 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+
+# padão de resposta do chat bot em mensagens
+pares = [
+    [
+        r"oi|olá|e ai",
+        ["Oi, tudo bem?", "Olá como posso ajudar você?"]
+    ],
+    [
+        r"qual é o seu nome?",
+        ["Meu nome é ChatBot. E o seu?",]
+    ],
+    [
+        r"qual é o seu nome?",
+        ["Meu nome é ChatBot e o seu?",]
+    ],
+    [
+        r"como você está?",
+        ["Estou bem, obrigado por perguntar.", ]
+    ],
+    [
+        r"adeus",
+        ["Adeus! Volte logo."]
+    ],
+]
+
+# criando o chatbot
+
+def chatbot():
+    print("Olá! Eu sou um chatbot. Como posso ajudar você hoje? Digite 'adeus' para sair.")
+    chat = Chat(pares, reflections)
+    chat.converse()
+
+
+if __name__ == "__main__":
+    chatbot()
+
+
+
+
+
